370fd1835f628a7ac35689194174341e2e64f4ba
Chaninat40 <32437912+Chaninat40@users.noreply.github.com>
2017-11-26 21:28:32 +0700

    update

---
+++ list.js
@@ -0,0 +1,20 @@
+axios.get('http://localhost:3000/users')
+.then(function(response) {
+    for (var i = 0; i < response.data.length; i++) {
+        var item = response.data[i];
+
+        $('#data').append(
+            '<tr><th scope="row" id="num"><center>' + item.id + '</center></th>' +
+            '<td id="fname">' + item.first_name + '</td>' +
+            '<td id="lname">' + item.last_name + '</td>' +
+            '<td id="role">' + item.role + '</td>' +
+            '<td id="exp">' + item.expired + '</td></tr>'
+
+
+        );
+    }
+    console.log(response);
+})
+.catch(function(error) {
+    console.log(error);
+});
